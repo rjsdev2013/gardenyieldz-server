2290ac7915b7a55c4de58da2c4807933eacd67e2
Ryan Stahr <ryan.j.stahr@gmail.com>
2022-09-03 09:40:03 -0500

    fixed indentations;added destroy to journalView

--- gardenyieldzapi/views/JournalView.py
+++ gardenyieldzapi/views/JournalView.py
@@ -21,7 +21,35 @@ class JournalView(ViewSet):
         journals = Journal.objects.all()
         serializer = PlantSerializer(journals, many=True)
         return Response(serializer.data)
+        
+    def create(self, request):
+        gardener = Gardener.objects.get(user=request.auth.user)
+        plant_id = Plant.objects.get(pk=request.data["plant_id"])
+    
+        journal = Journal.objects.get(user=request.auth.user)
+        serializer = CreateJournalSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(journal=journal)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def update(self, request, pk):
+        """Handle PUT requests for a game"""
+        journal = Journal.objects.get(pk=pk)
+        journal.date = request.data["date"]
+        journal.fruitNumber = request.data["fruitNumber"]
+        journal.weight = request.data["weight"]
+        journal.plant_id = request.data["plant_id"]
+        journal.gardener_id = request.data["gardener_id"]
+
+        journal.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        journal = Journal.objects.get(pk=pk)
+        journal.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+        
 class JournalSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
@@ -30,42 +58,14 @@ class JournalSerializer(serializers.ModelSerializer):
         fields = ('id', 'date', 'fruitNumber', 'weight','plant_id','gardener_id')
         depth = 1
         
-def create(self, request):
-    """Handle POST operations
-
-    Returns
-        Response -- JSON serialized game instance
-    """
-    gardener = Gardener.objects.get(user=request.auth.user)
-    plant_id = Plant.objects.get(pk=request.data["plant_id"])
-
-    journal = Journal.objects.create(
-        date=request.data["date"],
-        fruitNumber=request.data["fruitNumber"],
-        weight=request.data["weight"],
-        plant_id=request.data["plant_id"],
-        gardener=gardener,
-        plant_id=plant_id
-    )
-    serializer = JournalSerializer(journal)
-    return Response(serializer.data)
-
-# 8/30 added journalView create function;initiated git;next is edit and then delete(ch10)
-
-def update(self, request, pk):
-    """Handle PUT requests for a game
-
-    Returns:
-        Response -- Empty body with 204 status code
-    """
-
-    journal = Journal.objects.get(pk=pk)
-    journal.date = request.data["date"]
-    journal.fruitNumber = request.data["fruitNumber"]
-    journal.weight = request.data["weight"]
-    journal.plant_id = request.data["plant_id"]
-    journal.gardener_id = request.data["gardener_id"]
-
-    journal.save()
-
-    return Response(None, status=status.HTTP_204_NO_CONTENT)
+class CreateJournalSerializer (serializers.ModelSerializer):
+    class Meta: 
+        model = Journal
+        fields = (
+            'id',
+            'date',
+            'fruitNumber',
+            'weight',
+            'plant_id',
+            'gardener_id'   
+        )
